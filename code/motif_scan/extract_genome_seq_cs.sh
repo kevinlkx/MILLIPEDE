fc60004eb672ceb2c54fdf7352bc066948bca22b
Kevin Luo <kevinlkx@gmail.com>
2017-08-04 12:59:13 -0500

    remove echosd

--- code/motif_scan/extract_genome_seq_cs.sh
+++ code/motif_scan/extract_genome_seq_cs.sh
@@ -4,7 +4,7 @@ genome_dir="/home/home4/kevinluo/MNaseData/Model/PWM/genomeseq/sacCer2_2008"
 
 ## for each chr, extract the genome sequence and save in the database
 for chr_num in {1..16}; do
-  echo perl extractGenomeSeq.pl \
+  perl extractGenomeSeq.pl \
   --seqfile=${genome_dir}/chr${chr_num}.fa \
   --chrNum=${chr_num}
 done
